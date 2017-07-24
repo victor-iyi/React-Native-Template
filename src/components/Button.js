2b3a5b6ecfbc4d85ce2e6cee6e558f1689685a53
victor-iyiola <javafolabi@gmail.com>
2017-07-24 01:03:45 +0100

    Refactored components to functional component

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,21 +1,17 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { View, Text, TouchableNativeFeedback, Platform } from 'react-native';
 import { styles } from '../styles';
 
-class Button extends Component {
-
-  render() {
-    return (
-      <TouchableNativeFeedback
-          background={ Platform.OS === 'android' ? TouchableNativeFeedback.SelectableBackground() : '' }
-          onPress={this.props.onPress}>
-        <View style={styles.button}>
-          <Text style={styles.buttonText}>{this.props.title}</Text>
-        </View>
-      </TouchableNativeFeedback>
-    );
-  }
-
-}
+const Button = (props) => {
+  return (
+    <TouchableNativeFeedback
+        background={ Platform.OS === 'android' ? TouchableNativeFeedback.SelectableBackground() : '' }
+        onPress={props.onPress}>
+      <View style={styles.button}>
+        <Text style={styles.buttonText}>{props.title}</Text>
+      </View>
+    </TouchableNativeFeedback>
+  );
+};
 
 export default Button;
